064975a6c03a825d19843e479e1b8efe53e8b2ce
matia <matiassematni@gmail.com>
2024-10-06 11:32:02 +0200

    Revert "Bereistellung einer Create Methoden"
    This reverts commit 7b8557f7

--- src/org/hbrs/se1/ws24/exercises/uebung1/control/Factory.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/control/Factory.java
@@ -3,11 +3,7 @@ package org.hbrs.se1.ws24.exercises.uebung1.control;
 public class Factory {
 
     public Translator createTranslator() {
-        return new GermanTranslator();
-    }
-
-    public void create(){
-
+        new GermanTranslator();
     }
 
 
