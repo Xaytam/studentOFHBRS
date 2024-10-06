2cec08ffaaff5e1d3d8ffbc52110f007cf155bb3
matia <matiassematni@gmail.com>
2024-10-06 11:41:58 +0200

    Eine weitere experimentelle Methode

--- src/org/hbrs/se1/ws24/exercises/uebung1/control/Factory.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/control/Factory.java
@@ -3,7 +3,7 @@ package org.hbrs.se1.ws24.exercises.uebung1.control;
 public class Factory {
 
     public Translator createTranslator() {
-        new GermanTranslator();
+        return new GermanTranslator();
     }
 
 
