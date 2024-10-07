ab08b72f39148485b6fcdbed0a26999971f3af61
matia <matiassematni@gmail.com>
2024-10-07 18:14:34 +0200

    Fast fertig aber bin nicht sicher mit die Excpetions teil

--- src/org/hbrs/se1/ws24/exercises/uebung1/control/GermanTranslator.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/control/GermanTranslator.java
@@ -4,24 +4,28 @@ public class GermanTranslator implements Translator {
 
 	public String date = "Okt/2024"; // Default-Wert
 	public String[] zahlen = new String[10];
-
+	public GermanTranslator(){
+	zahlen[0] = "eins";
+	zahlen[1] = "zwei";
+	zahlen[2] = "drei";
+	zahlen[3] = "vier";
+	zahlen[4] = "fünf";
+	zahlen[5] = "sechs";
+	zahlen[6] = "sieben";
+	zahlen[7] = "acht";
+	zahlen[8] = "neun";
+	zahlen[9] = "zehn";}
 	/**
 	 * Methode zur Übersetzung einer Zahl in eine String-Repraesentation
 	 */
 	public String translateNumber( int number ) {
 		// [ihr Source Code aus Übung 1-2]
-		zahlen[0] = "eins";
-		zahlen[1] = "zwei";
-		zahlen[2] = "drei";
-		zahlen[3] = "vier";
-		zahlen[4] = "fünf";
-		zahlen[5] = "sechs";
-		zahlen[6] = "sieben";
-		zahlen[7] = "acht";
-		zahlen[8] = "neun";
-		zahlen[9] = "zehn";
+		try{
+		return zahlen[number-1];}
+		catch(ArrayIndexOutOfBoundsException e){
 
-		return zahlen[number-1];
+			throw new IllegalArgumentException("Übersetzung der Zahl " + number + " nicht möglich");
+		}
 	}
 
 	/**
